5f100a043dda2a679842236f66ad3c8f8bebffe6
luizotvio <luizotavio.03@outlook.com>
2024-05-03 20:26:35 -0300

    :sparkles: feat: laço de repetição for adiconado

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 let heroi = "Ceanna";
-let xpHeroi = 7.000;
-let nivelHeroi
+let xpHeroi = 10.000;
+const patenteHeroi = [
+    {nivelHeroi: 'Ferro', xpMin: 0, xpMax: 1.000},
+    {nivelHeroi: 'Bronze', xpMin: 1.001, xpMax: 2.000},
+    {nivelHeroi: 'Prata', xpMin: 2.001, xpMax: 5.000},
+    {nivelHeroi: 'Ouro', xpMin: 5.001, xpMax: 7.000},
+    {nivelHeroi: 'Platina', xpMin: 7.001, xpMax: 8.000},
+    {nivelHeroi: 'Ascendente', xpMin: 8.001, xpMax: 9.000},
+    {nivelHeroi: 'Imortal', xpMin: 9.001, xpMax: 10.000},
+    {nivelHeroi: 'Radiante', xpMin: 10.001, xpMax: Infinity}
+];
 
-if (xpHeroi <= 1.000) {
-    nivelHeroi = "Ferro";
-} else if (xpHeroi >= 1.001 && xpHeroi <= 2.000) {
-    nivelHeroi = "Bronze";
-} else if (xpHeroi >= 2.001 && xpHeroi <= 5.000) {
-    nivelHeroi = "Prata";
-} else if (xpHeroi >= 5.001 && xpHeroi <= 7.000) {
-    nivelHeroi = "Ouro";
-} else if (xpHeroi >= 7.001 && xpHeroi <= 8.000) {
-    nivelHeroi = "Platina";
-} else if (xpHeroi >= 8.001 && xpHeroi <= 9.000) {
-    nivelHeroi = "Ascendente";
-} else if (xpHeroi >= 9.001 && xpHeroi <= 10.000) {
-    nivelHeroi = "Imortal";
-} else {
-    nivelHeroi = "Radiante";
-}
-
-console.log(`O Herói de nome ${heroi} está no nível de ${nivelHeroi}`);
+// usando um loop for para percorrer o array de objetos
+for(let i =0; i < patenteHeroi.length; i++) {
+    if(xpHeroi >= patenteHeroi[i].xpMin && xpHeroi <= patenteHeroi[i].xpMax) {
+        console.log(`O Herói de nome ${heroi} está no nível de ${patenteHeroi[i].nivelHeroi}`);
+        break;
+    }
+};
